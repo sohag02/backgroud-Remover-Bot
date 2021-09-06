7286a6ce286856c3bab89f768e3850da457b0ddb
sohag02 <70505164+sohag02@users.noreply.github.com>
2021-09-06 23:04:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     API_ID = 1927002
     API_HASH = "a5b3aa12e55b8183353a1039ccd109e1"
     BOT_TOKEN = "1877014826:AAG6ZW-Ma3dmZvKOvNhq8Gn7JLcQ1DSWB50"
-    REMOVE_BG_TOKEN = "7kQ6yp8aaCNy6fq6uJNsi4di"
+    REMOVE_BG_TOKEN = "ftNnTUHwUyALqXWDsmhNwq3P"
     MONGO = "mongodb+srv://rbgbot:rbgbot@cluster0.3grqz.mongodb.net/SJBots?retryWrites=true&w=majority"
